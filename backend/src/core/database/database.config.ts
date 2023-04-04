5c5640da072d35b4d2d15061cecf2f30a6850ed5
DamyanYara <124031495+DamyanYara@users.noreply.github.com>
2023-04-04 17:33:33 +0300

    setting up the database

---
+++ backend/src/core/database/database.config.ts
@@ -0,0 +1,16 @@
+import * as dotenv from 'dotenv';
+import { IDatabaseConfig } from './interfaces/dbConfig.interface';
+// Loads .env contents into process.env
+//Configuration in case we have also test and production, but in our case we have only development
+dotenv.config();
+
+export const databaseConfig: IDatabaseConfig = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME_DEVELOPMENT,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+  },
+};
---
+++ backend/src/core/database/interfaces/dbConfig.interface.ts
@@ -0,0 +1,12 @@
+// interface for the database configuration
+export interface IDatabaseConfigAttributes {
+  username?: string;
+  password?: string;
+  database?: string;
+  host?: string;
+  port?: number | string;
+  dialect?: string;
+}
+export interface IDatabaseConfig {
+  development: IDatabaseConfigAttributes;
+}
