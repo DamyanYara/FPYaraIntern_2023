ccc978722ff7d9fffd0679bd28498946b7929883
DamyanYara <124031495+DamyanYara@users.noreply.github.com>
2023-04-05 17:12:44 +0300

    creating login and signup methods

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Post, UseGuards, Request } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+import { UserDto } from '../users/dto/user.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+  // Validate method in local strategy by using email, username and password
+  @UseGuards(AuthGuard('local'))
+  @Post('login')
+  async login(@Request() req) {
+    return await this.authService.login(req.user);
+  }
+  // creating User and return JWT token
+  @Post('signup')
+  async signUp(@Body() user: UserDto) {
+    return await this.authService.create(user);
+  }
+}
