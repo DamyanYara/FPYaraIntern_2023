2cd570f72adc4b7749195d00153c618e9372c8ba
DamyanYara <124031495+DamyanYara@users.noreply.github.com>
2023-04-05 17:15:48 +0300

    update auth.service.ts

--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
   //check if user with provided email exists
   async validateUser(username: string, pass: string) {
     const user = await this.usersService.findOneByEmail(username);
@@ -18,6 +23,34 @@ export class AuthService {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { password, ...result } = user['dataValues'];
     return result;
+  }
+  // login the user, takes user info and token and returns token and user object
+  public async login(user) {
+    const token = await this.generateToken(user);
+    return { user, token };
+  }
+
+  public async create(user) {
+    const pass = await this.hashPassword(user.password); //hash the pass
+
+    const newUser = await this.usersService.create({ ...user, password: pass }); //create user
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password, ...result } = newUser['dataValues'];
+
+    const token = await this.generateToken(result); //generate token
+
+    return { user: result, token };
+  }
+
+  private async generateToken(user) {
+    const token = await this.jwtService.signAsync(user);
+    return token;
+  }
+
+  private async hashPassword(password) {
+    const hash = await bcrypt.hash(password, 10);
+    return hash;
   }
   // check the database with the provided from user password if match returs true
   private async comparePassword(enteredPassword, dbPassword) {
