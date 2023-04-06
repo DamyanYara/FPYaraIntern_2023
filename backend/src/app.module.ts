09ecd2c0217b6df115c35021d9ccfa5647a368dc
DamyanYara <124031495+DamyanYara@users.noreply.github.com>
2023-04-06 15:45:09 +0300

    updating the files

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,13 +5,19 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './core/database/database.module';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { ProductModule } from './modules/product/product.module';
+import { StockModule } from './modules/stock/stock.module';
+import { WarehouseModule } from './modules/warehouse/warehouse.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     UsersModule,
-    AuthModule, //Adding service to the Users module
+    AuthModule,
+    ProductModule,
+    WarehouseModule,
+    StockModule, //Adding service to the Users module
   ],
   controllers: [AppController],
   providers: [AppService],
--- backend/src/core/constants/index.ts
+++ backend/src/core/constants/index.ts
@@ -2,3 +2,6 @@ export const SEQUELIZE = 'SEQUELIZE';
 export const DEVELOPMENT = 'development';
 export const TEST = 'test';
 export const USER_REPOSITORY = 'USER_REPOSITORY';
+export const PRODUCT_REPOSITORY = 'PRODUCT_REPOSITORY';
+export const WAREHOUSE_REPOSITORY = 'PRODUCT_REPOSITORY';
+export const STOCK_REPOSITORY = 'STOCK_REPOSITORY';
