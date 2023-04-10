1169be9a600382260395528130cec739b5a3738a
DamyanYara <124031495+DamyanYara@users.noreply.github.com>
2023-04-10 14:40:13 +0300

    correction and update transfer module

--- backend/src/modules/transfer/transfer.module.ts
+++ backend/src/modules/transfer/transfer.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ProductModule } from '../product/product.module';
 import { TransferController } from './transfer.controller';
 import { transferProviders } from './transfer.providers';
 import { TransferService } from './transfer.service';
 
 @Module({
+  imports: [ProductModule],
   providers: [TransferService, ...transferProviders],
   exports: [TransferService],
   controllers: [TransferController],
