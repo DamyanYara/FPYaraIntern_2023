30c3ba78b22ee462532a636c73f7245f43a95017
DamyanYara <124031495+DamyanYara@users.noreply.github.com>
2023-04-10 14:40:53 +0300

    adding delete and find warehouse by user ID

--- backend/src/modules/warehouse/warehouse.controller.ts
+++ backend/src/modules/warehouse/warehouse.controller.ts
@@ -1,13 +1,45 @@
-import { Controller, Body, Post } from '@nestjs/common';
+import {
+  Controller,
+  Body,
+  Post,
+  Get,
+  UseGuards,
+  Request,
+  Delete,
+  Param,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { WarehouseDto } from './dto/warehouse.dto';
+import { Warehouse } from './warehouse.entity';
 import { WarehouseService } from './warehouse.service';
+import { User } from '../users/user.entity';
 
 @Controller('warehouse')
 export class WarehouseController {
   constructor(private warehouseService: WarehouseService) {}
 
+  // Using the users token from the create user, to authenticate for the creating of the warehouse
+  // akspecific user can create, delete, update specific warehouse
+  @UseGuards(AuthGuard('jwt'))
   @Post('create')
-  async create(@Body() warehouse: WarehouseDto) {
-    return await this.warehouseService.create(warehouse);
+  async create(
+    @Body() warehouse: WarehouseDto,
+    @Request() req,
+  ): Promise<Warehouse> {
+    return await this.warehouseService.create(warehouse, req.user.id);
+  }
+  // Retrieving all Wahrehouses By user Id after login token authentication
+  @UseGuards(AuthGuard('jwt'))
+  @Get()
+  async findAll(@Request() req): Promise<Warehouse[]> {
+    return await this.warehouseService.findAll(req.user.id);
+  }
+
+  // Edit
+  //Deleting warehouse by id, after token authentication from sertain user
+  @UseGuards(AuthGuard('jwt'))
+  @Delete(':id')
+  async deleteWarehouse(@Param('id') id: number, @Request() req) {
+    return await this.warehouseService.delete(id, req.user.id);
   }
 }
--- backend/src/modules/warehouse/warehouse.service.ts
+++ backend/src/modules/warehouse/warehouse.service.ts
@@ -2,6 +2,7 @@ import { Injectable, Inject } from '@nestjs/common';
 import { Warehouse } from './warehouse.entity';
 import { WarehouseDto } from './dto/warehouse.dto';
 import { WAREHOUSE_REPOSITORY } from 'src/core/constants';
+import { User } from '../users/user.entity';
 
 @Injectable()
 export class WarehouseService {
@@ -10,11 +11,20 @@ export class WarehouseService {
     private readonly warehouseRepository: typeof Warehouse,
   ) {}
 
-  async create(warehouse: WarehouseDto): Promise<Warehouse> {
-    return await this.warehouseRepository.create<Warehouse>(warehouse);
+  async create(warehouse: WarehouseDto, userId): Promise<Warehouse> {
+    return await this.warehouseRepository.create<Warehouse>({
+      ...warehouse,
+      userId,
+    });
   }
-  async findById(id: number): Promise<Warehouse> {
-    return await this.warehouseRepository.findOne<Warehouse>({ where: { id } });
+  async findAll(userId: number): Promise<Warehouse[]> {
+    return await this.warehouseRepository.findAll({
+      where: { userId },
+    });
   }
-  //TODO remove
+
+  async delete(id, userId) {
+    return await this.warehouseRepository.destroy({ where: { id, userId } });
+  }
+  //TODO DELETE And EDDIT
 }
